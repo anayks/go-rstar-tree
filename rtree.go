4dd521682bcfe977aea1186f2cc2912fe09aae13
anayks <anayks@yandex.ru>
2022-05-09 13:19:59 +0300

    coming back

--- rtree.go
+++ rtree.go
@@ -719,21 +719,20 @@ func (tree *Rtree) SearchIntersectWithLimit(k int, bb *Rect) []Spatial {
 }
 
 func (tree *Rtree) searchIntersect(results []Spatial, n *node, bb *Rect, filters []Filter) []Spatial {
-	if !n.leaf {
-		for _, v := range n.entries {
-			if intersect(bb, v.bb) {
-				tree.searchIntersect(results, v.child, bb, filters)
-			}
+	for _, v := range n.entries {
+		if !intersect(bb, v.bb) {
+			continue
 		}
-	} else {
-		for _, v := range n.entries {
-			if intersect(bb, v.bb) {
-				if v.obj == nil {
-					continue
-				}
-				results = append(results, v.obj)
-			}
+
+		if !n.leaf {
+			tree.searchIntersect(results, v.child, bb, filters)
 		}
+
+		if v.obj == nil {
+			continue
+		}
+
+		results = append(results, v.obj)
 	}
 	return results
 }
